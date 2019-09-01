e0ca986398e4addfebaef10caabbbc1d945c3906
Daniel <danihas@live.com>
2019-09-01 17:00:45 -0500

    pruebas para la implementacion del login

--- ERATO/ERATO/urls.py
+++ ERATO/ERATO/urls.py
@@ -14,15 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pages import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
 	path('home/',views.home_sw, name='home_sw'),
 	path('',views.login, name='home'),
-    path('admin/', admin.site.urls),
-   	 
+        path('admin/', admin.site.urls),
+        path('accounts/', include('django.contrib.auth.urls')),
+
 ]
 
 urlpatterns +=staticfiles_urlpatterns()
