aa463374787af5d3f103ad6df7bdc64dd61d55d4
Gustavo Mendez <gamendez98@gmail.com>
2019-09-03 11:52:14 -0500

    create models for SW and Service(not finished)

--- ERATO/pages/models.py
+++ ERATO/pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+class SW(models.Model):
+    email = models.CharField(max_length=200)
+    user_name=models.CharField(max_length=200)
+    picture_path=models.CharField(max_length=200)#relative to Erato/ERATO
+    MC_path=models.CharField(max_length=200)#relative to Erato/ERATO
+    birthDate=models.DateTimeField('date published')
+    statue=models.CharField(max_length=20)
+    eye_color=models.CharField(max_length=20)
+
+
+class Service(models.Model):
+    sw = models.ForeignKey(SW, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description=models.CharField(max_length=400)
+    #TODO tags=hay que ver como hacer relacion muchos a muchos en django
+    price=models.IntegerField()
 # Create your models here.
