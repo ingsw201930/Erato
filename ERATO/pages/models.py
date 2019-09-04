d96584ee4efe25c40bb8b8c3536102d12d1fe2be
Gustavo Mendez <gamendez98@gmail.com>
2019-09-04 13:17:24 -0500

    now there is an admin page /admin(you need to create a super user to use it) the home_sw page is now home/<user_name> and is dynamic SW has a user that is it's PK

--- ERATO/pages/models.py
+++ ERATO/pages/models.py
@@ -4,12 +4,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class SW(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
     picture_path=models.CharField(max_length=200)#relative to Erato/ERATO
     MC_path=models.CharField(max_length=200)#relative to Erato/ERATO
     birthDate=models.DateTimeField('date birth')
-    statue=models.CharField(max_length=20)
+    status=models.CharField(max_length=20)
     eye_color=models.CharField(max_length=20)
+
 
 
 class Service(models.Model):
--- ERATO/pages/views.py
+++ ERATO/pages/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import SW
 import os
 
 # Create your views here.
@@ -8,6 +10,14 @@ def login(request):
 	return render(request, "registration/login.html", {'path':mypath})
 	#return render_to_response('ERATO/web/login.html')
 
-def home_sw(request):
+#user_name is a parameter that comes from the url ex:home/gustavo ->username=gustavo
+def home_sw(request,user_name):
 	mypath=os.path
-	return render(request, "home.html", {'path':mypath})
+	try:
+		user=User.objects.get(username=user_name)#get user from U_N
+		sw=SW.objects.get(user=user)
+	except User.DoesNotExist:
+		raise Http404("Question does not exist")
+	except SW.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, "home.html", {'sw':sw})
