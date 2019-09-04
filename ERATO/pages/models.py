22d71f80c5550d5e3d2dc03b415f56edda293b6e
Gustavo Mendez <gamendez98@gmail.com>
2019-09-04 11:14:22 -0500

    models modified

--- ERATO/pages/models.py
+++ ERATO/pages/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class SW(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
