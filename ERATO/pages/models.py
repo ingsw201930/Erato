edbf4ec4ed897fdc90ee919d53aa2e5f3ded2142
Gustavo Mendez <gamendez98@gmail.com>
2019-09-04 11:10:36 -0500

    modify SW model

--- ERATO/pages/models.py
+++ ERATO/pages/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
 class SW(models.Model):
-    email = models.CharField(max_length=200)
-    user_name=models.CharField(max_length=200)
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
     picture_path=models.CharField(max_length=200)#relative to Erato/ERATO
     MC_path=models.CharField(max_length=200)#relative to Erato/ERATO
     birthDate=models.DateTimeField('date published')
     statue=models.CharField(max_length=20)
     eye_color=models.CharField(max_length=20)
+@receiver(post_save,sender=User)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        SW.objects.create(user=instance)
 
+@reciever(post_save,sender=User)
+def save_user_profile(sender,instance,created,**kwargs):
+    instance.profile.save()
 
 class Service(models.Model):
     sw = models.ForeignKey(SW, on_delete=models.CASCADE)
