5e628d859876b2a83f7f2f9aaf186fe75ad08d4d
ValentinaY <valyatev@gmail.com>
2019-10-29 20:12:34 -0500

    Attaching files fixed

--- ERATO/app_client/views.py
+++ ERATO/app_client/views.py
@@ -39,7 +39,8 @@ def signup(request):
     if request.method == 'POST':
         form = ClientSignUpForm(request.POST)
         form_ul = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid() and form_ul.is_valid():
+        form_mc = UploadMCForm(request.POST, request.FILES)
+        if form.is_valid() and form_mc.is_valid() and form_ul.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
@@ -48,7 +49,8 @@ def signup(request):
                 user=user,
                 email=form.cleaned_data.get('email')
             )
-            handle_uploaded_file(request.FILES['file'], username)
+            handle_uploaded_file(request.FILES['file'], username, 'PPSW')
+            handle_uploaded_file(request.FILES['mc'], username, 'MC')
             client.save()
             login(request, user)
             return HttpResponseRedirect('/c/home/')
@@ -56,6 +58,14 @@ def signup(request):
         form = ClientSignUpForm()
     return render(request, 'signup_c/signup_c.html', {'form': form})
 
+def handle_uploaded_file(f, username, code):
+    if code == 'PPSW':
+        file_name = "assets/images/pro_pics/%s" % hashlib.md5((username+erato_key).encode()).hexdigest()
+    if code == 'MC':
+        file_name = "assets/mc/%s" % hashlib.md5((username+erato_key).encode()).hexdigest()
+    with open(file_name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 
 # Me seeing my own profile
 @login_required_client
