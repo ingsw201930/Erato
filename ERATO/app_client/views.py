eafda3eb640e1507863abf86997dc27ab6af14e4
gamendez98 <gamendez98@gmail.com>
2019-11-09 21:04:46 -0500

    Filter started

--- ERATO/app_client/views.py
+++ ERATO/app_client/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth import login
 from .decorators import login_required_client
 import hashlib
 from ERATO.settings import BASE_DIR
+from django.db.models import Q
 # Create your views here.
 # Home for clients
 erato_key= "er"
@@ -27,7 +28,10 @@ def home_c(request):
 @login_required_client
 def get_service_list(request,index):
     n=5
-    services=Service.objects.all()[index*n:(index+1)*n]
+    search=request.GET.get("search",None)
+    services=Service.objects.filter(
+        Q(description__icontains=search)|Q(name__icontains=search)
+        )[index*n:(index+1)*n]
     return render(request,'home_c/services.html',{'services':services})
 
 def signupform(request):
--- ERATO/templates/home_c/home.js
+++ ERATO/templates/home_c/home.js
@@ -1,9 +1,15 @@
 
 var current_index=0
 
+
+
 function get_service_list(index){
-    $.ajax({url: "{{BASEURL}}/c/get_service_list/"+index, 
-    success: function(result){
+    $.ajax({
+        url: "{{BASEURL}}/c/get_service_list/"+index,
+        data:{
+            search:document.getElementById("filter_search").value
+        },
+        success: function(result){
         container=document.getElementById("service_container")
         container.innerHTML = result;
     }});
@@ -21,4 +27,10 @@ function back(){
         document.getElementById("current_index").innerHTML=current_index+1;
         get_service_list(current_index)
     }
+}
+
+function filter(){
+    current_index=0
+    document.getElementById("current_index").innerHTML=current_index+1;
+    get_service_list(current_index)
 }
