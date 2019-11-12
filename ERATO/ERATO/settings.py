05fd53fc94ae54b448447dafb417cd5d4f95275e
donadol <ldonado11@gmail.com>
2019-11-12 16:59:02 -0500

    Pagination buttons centered

--- ERATO/ERATO/settings.py
+++ ERATO/ERATO/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-url_base = 'http://192.168.0.7:8000'
+url_base = 'http://localhost:8000'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
